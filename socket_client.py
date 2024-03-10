0bb6e84ffe0361636e56c05d59388fe122bf0a67
Yuval Dekel <yuvddd05@gmail.com>
2024-03-10 22:20:19 +0200

    create Nicer code

--- socket_client.py
+++ socket_client.py
@@ -3,16 +3,20 @@ import socket
 def main():
     try:
         my_socket = socket.socket()
+        
         what_to_send = input("what would you like to send to the server: ")
+        
         my_socket.connect(("127.0.0.1", 8820))
         my_socket.send(what_to_send.encode())
         data = my_socket.recv(1024).decode()
+
         if what_to_send != '' and data == '':
-            raise Exception
+            raise Exception("server and client disconnected")
         print(data)
         
-    except:
-        print("Error receiving data back")
+    except Exception as e:
+        print("Error: {}".format(e))
+        
     finally:
         my_socket.close()
 
