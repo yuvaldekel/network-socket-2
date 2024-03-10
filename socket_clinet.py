1bc9bce70dee526e7d7023dcea9daaae49960189
Yuval Dekel <yuvddd05@gmail.com>
2024-03-10 13:37:06 +0200

    add server pyc

--- socket_clinet.py
+++ socket_clinet.py
@@ -1,12 +1,18 @@
 import socket
 
 def main():
-    with socket.socket() as my_socket:
+    try:
+        my_socket = socket.socket()
+        what_to_send = input("what would you like to send to the server: ")
         my_socket.connect(("127.0.0.1", 8820))
-        my_socket.send("Omer".encode())
+        my_socket.send(what_to_send.encode())
         data = my_socket.recv(1024).decode()
         print(data)
-
+        
+    except ConnectionRefusedError:
+        print("Error receiving data back")
+    finally:
+        my_socket.close()
 
 if __name__ == "__main__":
     main()
